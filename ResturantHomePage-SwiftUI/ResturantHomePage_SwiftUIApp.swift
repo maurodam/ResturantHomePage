43a6f2a9ec0ae728b87b7a1e38729b04e3181a8f
Damiano <damiano.mauro@hotmail.com>
2021-12-13 19:24:43 +0100

    Initial Commit

---
+++ ResturantHomePage-SwiftUI/ResturantHomePage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResturantHomePage_SwiftUIApp.swift
+//  ResturantHomePage-SwiftUI
+//
+//  Created by Damiano Mauro on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ResturantHomePage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
